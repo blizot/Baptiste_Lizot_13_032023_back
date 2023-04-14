808434fc9c3d1d5c535da3ca96aacc20374c9571
emnificent <github.com/emnificent>
2023-04-15 01:22:41 +0200

    feat: add API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+import * as docs from './docs/openapi.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  SwaggerModule.setup('api/v1/docs', app, docs as any);
+
   await app.listen(5000);
 }
 bootstrap();
